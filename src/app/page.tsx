4aa2f30b7b2ec7b93905c8c09f83138a7613c7a5
Eduardo Fawke <teduardof@gmail.com>
2025-07-24 14:38:01 +0100

    job title on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <Flex justify="between" height="100%" direction="column">
             <Flex gap="4" direction="column">
               <Heading mt="6" size="9">Ted Fawke</Heading>
-              <Heading weight="medium" size="6">Some words about coding</Heading>
+              <Heading weight="medium" size="6">Full-Stack Developer</Heading>
               <Flex direction="row" mt="8">
                 <Image
                   src="/pixil-frame-0 (7).png"
