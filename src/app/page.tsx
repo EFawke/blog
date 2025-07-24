def6abdd4c66720910fc3290e6a3ee741d94212d
Eduardo Fawke <teduardof@gmail.com>
2025-07-24 05:10:57 +0100

    cute knight guy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Links } from "./ui/links"
 import { BlogNav } from "./ui/blogNav"
 import { Suspense } from 'react';
 import { BlogNavSkeleton } from './ui/skeletons';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -10,21 +11,27 @@ export default function Home() {
       <Flex gap="4" direction="row" justify="between" id="weird_flex_bro">
         <Section id="left_page" top="0">
           <Flex justify="between" height="100%" direction="column">
-            <HoverCard.Root>
-              <HoverCard.Trigger>
-                <Flex gap="4" direction="column">
-                  <Heading mt="6" size="9">Ted Fawke</Heading>
-                  <Heading weight="medium" size="6">Some words about coding</Heading>
-                </Flex>
-              </HoverCard.Trigger>
-            </HoverCard.Root>
+            <Flex gap="4" direction="column">
+              <Heading mt="6" size="9">Ted Fawke</Heading>
+              <Heading weight="medium" size="6">Some words about coding</Heading>
+              <Flex gap="4" direction="row" align="end">
+              
+              <Image
+                src="/reading.png" // this references /public/projects.png
+                alt="Projects"
+                width={240}         // specify width
+                height={240}        // and height
+              />
+              {/* <Heading weight="medium" size="6">Recent Posts</Heading> */}
+              </Flex>
+            </Flex>
             <Links />
           </Flex>
         </Section>
         <Section id="right_page">
           <Flex gap="4" direction="column">
-            <Suspense fallback={ <BlogNavSkeleton/> }>
-              <BlogNav/>
+            <Suspense fallback={<BlogNavSkeleton />}>
+              <BlogNav />
             </Suspense>
           </Flex>
         </Section>
