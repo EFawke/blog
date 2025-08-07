d29f5682893cd01b4c3c81ac33e5dfa86d293e1c
Eduardo Fawke <teduardof@gmail.com>
2025-08-07 02:48:19 +0100

    removed little guy :(

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Links } from "./ui/links"
 import { BlogNav } from "./ui/blogNav"
 import { Suspense } from 'react';
 import { BlogNavSkeleton } from './ui/skeletons';
-import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -13,15 +12,7 @@ export default function Home() {
           <Flex justify="between" height="100%" direction="column">
             <Flex gap="4" direction="column">
               <Heading mt="6" size="9">Ted Fawke</Heading>
-              <Heading weight="medium" size="6">Full-Stack Developer</Heading>
-              <Flex direction="row" mt="8">
-                <Image
-                  src="/pixil-frame-0 (7).png"
-                  alt="Projects"
-                  width={130}
-                  height={230}
-                />
-              </Flex>
+              <Heading weight="medium" size="6">Some words about coding</Heading>
             </Flex>
             <Links />
           </Flex>
